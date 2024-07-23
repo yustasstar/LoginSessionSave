8a35dc2758180f7f7d16569ededf52ad7ee52384
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-07-23 23:28:54 +0300

    start

---
+++ UserSessionSave.cs
@@ -0,0 +1,18 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class UserSessionSave : PageTest
+{
+    [Test]
+    public async Task UserSession()
+    {
+
+    }
+}
